0d644ca50fe3a7d964203fc8d330b7ff7ffbb9c8
Festival3224 <QSR^V9&h>
2024-10-22 15:52:22 +0200

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   //base: 'frontend-mentor-minimalistic-portfolio',
+  base: 'frontend-mentor-minimalistic-portfolio',
   plugins: [
     vue(),
     svgLoader({ })
